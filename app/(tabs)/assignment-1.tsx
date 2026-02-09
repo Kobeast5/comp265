73e216943b94e8f4b1cc26c6e1d77a9e9ec0d5d1
Kobe Curtis <KobeCurtis703@gmail.com>
2026-02-09 15:09:08 -0600

    Post Info Bar
    - Created a bar containing basic information about a post (poster username, post title, user profile picture).
    - Added an image for a profile picture.

--- app/(tabs)/assignment-1.tsx
+++ app/(tabs)/assignment-1.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 const ArtworkApp = () => {
     return (
 
         <View style={styles.container}>
 
-            <Text style={styles.title}>This is some text.</Text>
+            <View style={styles.topbar}></View>
+
+            <View style={styles.postInfo}>
+
+                <Image source={require('../../media/example-profile-picture.jpeg')} style={styles.profilePicture} />
+
+                <View style={styles.usernameAndTitle}>
+
+                    <Text style={styles.title}>My Artwork</Text>
+
+                    <Text style={styles.username}>Kobeast</Text>
+
+                </View>
+
+            </View>
 
         </View>
 
@@ -21,10 +35,38 @@ const styles = StyleSheet.create({
         backgroundColor: '#f0f0f0',
     },
 
+    topbar: {
+        position: 'absolute',
+        top: 0,
+        backgroundColor: '#73A3D3',
+        width: 400,
+        height: 50,
+    },
+
+    postInfo: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+
+    profilePicture: {
+        width: 62,
+        height: 62,
+        borderRadius: 100,
+    },
+
+    usernameAndTitle: {
+        paddingLeft: 10,
+    },
+
     title: {
         fontSize: 16,
-        fontWeight: 'bold',
-    }
+        fontWeight: 600,
+    },
+
+    username: {
+        fontSize: 21,
+        fontWeight: 600,
+    },
 })
 
 export default ArtworkApp;
